8398fc163f9639c8eb6ab4c931cfb4c94fa3f909
adzuki1 <mneto.fis@gmail.com>
2024-01-24 00:35:26 -0300

    Updated documentation

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 
 int main(){
 
+    // Window object
     Window window("SDL Tutorial", 800, 600);
 
+    // look for events, while window is not closed
     while(!window.isClosed()){
         window.pollEvents();
     }
